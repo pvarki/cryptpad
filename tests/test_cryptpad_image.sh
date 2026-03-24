1b09afc0cad9c71a25cc66e2d27a6a9d0382645a
elderx <elderx1@proton.me>
2026-03-24 14:55:41 +0200

    fix: allow drawio pages to use pad CSP

--- tests/test_cryptpad_image.sh
+++ tests/test_cryptpad_image.sh
@@ -7,6 +7,8 @@ docker build -f docker/cryptpad/Dockerfile -t "${IMAGE_TAG}" .
 docker run --rm "${IMAGE_TAG}" sh -lc '
   test -d /cryptpad/lib/plugins/sso &&
   test -f /cryptpad/config/sso.js &&
+  grep -Fq "    /^\\/(sheet|presentation|doc|diagram)\\/inner\\.html.*/," /cryptpad/lib/http-worker.js &&
+  grep -Fq "    /^\\/components\\/drawio\\/src\\/main\\/webapp\\/index\\.html.*/," /cryptpad/lib/http-worker.js &&
   mkdir -p /cryptpad/data/decrees &&
   /usr/local/bin/cryptpad-entrypoint.sh true &&
   grep -q "\"SET_BEARER_SECRET\"" /cryptpad/data/decrees/decree.ndjson
